7d5719d6d7023a453b7fdb7bd92b1949d92ee380
ryansoe <ryansoe90686@gmail.com>
2024-11-10 17:32:03 -0800

    auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })) // to parse form data
+
+app.use(cookieParser()) // parse req to get cookies
 
 app.use("/api/auth", authRoutes);
 
