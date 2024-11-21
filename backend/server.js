05f462af5d8104eba15c7f1fb7815087ef9b4a67
ryansoe <ryansoe90686@gmail.com>
2024-11-20 23:00:29 -0800

    payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json()); // to parse req.body
+app.use(express.json({limit:"5mb"})); // to parse req.body
 app.use(express.urlencoded({ extended: true })) // to parse form data
 
 app.use(cookieParser()) // parse req to get cookies
--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -2,53 +2,53 @@ import { CiImageOn } from "react-icons/ci";
 import { BsEmojiSmileFill } from "react-icons/bs";
 import { useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { toast } from 'react-hot-toast'
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
 
 const CreatePost = () => {
 	const [text, setText] = useState("");
 	const [img, setImg] = useState(null);
 	const imgRef = useRef(null);
 
-	const {data:authUser} = useQuery({queryKey: ['authUser']})
-	const queryClient = useQueryClient()
+	const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+	const queryClient = useQueryClient();
 
-	const {mutate:createPost, isPending, isError,error} = useMutation({
-		mutationFn: async ({text,img}) => {
+	const {
+		mutate: createPost,
+		isPending,
+		isError,
+		error,
+	} = useMutation({
+		mutationFn: async ({ text, img }) => {
 			try {
 				const res = await fetch("/api/posts/create", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
 					},
-					body: JSON.stringify({text,img})
-				})
-				const data = await res.json()
-				if(!res.ok){
-					throw new Error(data.error || "Something went wrong")
+					body: JSON.stringify({ text, img }),
+				});
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
 				}
-				return data
+				return data;
 			} catch (error) {
-				throw new Error(error)
+				throw new Error(error);
 			}
 		},
 
 		onSuccess: () => {
-			setText("")
-			setImg(null)
-			toast.success("Post created successfully")
-			queryClient.invalidateQueries({queryKey: ['posts']})
-		}
-	})
-
-
-	const data = {
-		profileImg: "/avatars/boy1.png",
-	};
+			setText("");
+			setImg(null);
+			toast.success("Post created successfully");
+			queryClient.invalidateQueries({ queryKey: ["posts"] });
+		},
+	});
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		createPost({text, img})
+		createPost({ text, img });
 	};
 
 	const handleImgChange = (e) => {
@@ -102,9 +102,7 @@ const CreatePost = () => {
 						{isPending ? "Posting..." : "Post"}
 					</button>
 				</div>
-				{isError && <div className='text-red-500'>
-					{error.message}
-					</div>}
+				{isError && <div className='text-red-500'>{error.message}</div>}
 			</form>
 		</div>
 	);
